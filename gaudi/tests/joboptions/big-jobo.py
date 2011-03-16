671c30868885de30cfe78fac7953fb55d2cfcedd
Sebastien Binet <binet@farnsworth>
2011-03-16 10:33:24 +0100

    fix imports to point at bitbucket

--- gaudi/tests/joboptions/big-jobo.py
+++ gaudi/tests/joboptions/big-jobo.py
@@ -2,51 +2,106 @@ app.props.EvtMax = 10000
 app.props.OutputLevel = 1
 #app.props.NbrProcs = 1
 
-app.svcs += Svc("gaudi/kernel/evtproc:evtproc",
-                "evt-proc",
-                OutputLevel=Lvl.INFO,
-                NbrWorkers=5000)
+app.svcs += Svc(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/kernel/evtproc:evtproc",
+    "evt-proc",
+    OutputLevel=Lvl.INFO,
+    NbrWorkers=5000
+    )
 
-app.algs += Alg("gaudi/tests/pkg1:alg1", "alg1", OutputLevel=Lvl.ERROR)
-app.algs += Alg("gaudi/tests/pkg1:alg2", "alg2", OutputLevel=Lvl.ERROR)
-app.algs += Alg("gaudi/tests/pkg2:alg1", "alg_one", OutputLevel=Lvl.ERROR)
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg1:alg1",
+    "alg1",
+    OutputLevel=Lvl.ERROR
+    )
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg1:alg2",
+    "alg2",
+    OutputLevel=Lvl.ERROR
+    )
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg1",
+    "alg_one",
+    OutputLevel=Lvl.ERROR
+    )
 
-app.svcs += Svc("gaudi/tests/pkg1:svc1", name="svc1", OutputLevel=Lvl.ERROR)
-app.svcs += Svc("gaudi/tests/pkg2:svc2", "svc2")
+app.svcs += Svc(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg1:svc1",
+    name="svc1",
+    OutputLevel=Lvl.ERROR
+    )
+app.svcs += Svc(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:svc2",
+    "svc2"
+    )
 
-app.svcs += Svc("gaudi/kernel/datastore:datastoresvc", "evt-store")
-app.svcs += Svc("gaudi/kernel/datastore:datastoresvc", "det-store")
+app.svcs += Svc(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/kernel/datastore:datastoresvc",
+    "evt-store"
+    )
+app.svcs += Svc(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/kernel/datastore:datastoresvc",
+    "det-store"
+    )
 
-app.algs += Alg("gaudi/tests/pkg2:alg_adder", "adder_1",
-                OutputLevel=Lvl.ERROR,
-                Val=0.)
-app.algs += Alg("gaudi/tests/pkg2:alg_dumper", "dumper-1",
-                ExpectedValue=1)
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg_adder",
+    "adder_1",
+    OutputLevel=Lvl.ERROR,
+    Val=0.
+    )
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg_dumper",
+    "dumper-1",
+    ExpectedValue=1
+    )
 
-app.algs += Alg("gaudi/tests/pkg2:alg_adder", "adder_2",
-                OutputLevel=Lvl.ERROR,
-                Val=3.)
-app.algs += Alg("gaudi/tests/pkg2:alg_dumper", "dumper-2",
-                ExpectedValue=2)
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg_adder",
+    "adder_2",
+    OutputLevel=Lvl.ERROR,
+    Val=3.
+    )
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg_dumper",
+    "dumper-2",
+    ExpectedValue=2
+    )
 
-app.algs += Alg("gaudi/tests/pkg2:alg_adder", "adder_3")
-app.algs += Alg("gaudi/tests/pkg2:alg_dumper", "dumper-3",
-                ExpectedValue=3)
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg_adder",
+    "adder_3"
+    )
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg_dumper",
+    "dumper-3",
+    ExpectedValue=3
+    )
 
-app.algs += Alg("gaudi/tests/pkg2:alg_dumper", "dumper",
-                NbrJets = "njets",
-                ExpectedValue=3,
-                OutputLevel=Lvl.ERROR)
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg_dumper",
+    "dumper",
+    NbrJets = "njets",
+    ExpectedValue=3,
+    OutputLevel=Lvl.ERROR
+    )
 
 if 1:
     for i in xrange(500):
-        app.algs += Alg("gaudi/tests/pkg2:alg_adder",
-                        "addr--%04i" % i,
-                        SimpleCounter="my_counter")
-        app.algs += Alg("gaudi/tests/pkg2:alg_dumper",
-                        "dump--%04i" % i,
-                        SimpleCounter="my_counter",
-                        ExpectedValue=i+1)
+        app.algs += Alg(
+            "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg_adder",
+            "addr--%04i" % i,
+            SimpleCounter="my_counter"
+            )
+        app.algs += Alg(
+            "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg_dumper",
+            "dump--%04i" % i,
+            SimpleCounter="my_counter",
+            ExpectedValue=i+1
+            )
     
-app.toolsvc += Tool("gaudi/tests/pkg1:tool1", name="tool1")
+app.toolsvc += Tool(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg1:tool1",
+    name="tool1"
+    )
 
--- gaudi/tests/joboptions/test-outstreams.py
+++ gaudi/tests/joboptions/test-outstreams.py
@@ -5,50 +5,79 @@ app.props.OutputLevel = 1
 nworkers = 50
 #nworkers = 1
 
-app.svcs += Svc("gaudi/kernel/evtproc:evtproc",
-                "evt-proc",
-                OutputLevel=Lvl.INFO,
-                NbrWorkers=nworkers)
+app.svcs += Svc(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/kernel/evtproc:evtproc",
+    "evt-proc",
+    OutputLevel=Lvl.INFO,
+    NbrWorkers=nworkers
+    )
 
-app.svcs += Svc("gaudi/kernel/datastore:datastoresvc", "evt-store")
-app.svcs += Svc("gaudi/kernel/datastore:datastoresvc", "det-store")
-app.svcs += Svc("gaudi/kernel/outstream:json_outstream_svc",
-                "outstreamsvc",
-                OutputLevel=Lvl.INFO)
+app.svcs += Svc(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/kernel/datastore:datastoresvc",
+    "evt-store"
+    )
+app.svcs += Svc(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/kernel/datastore:datastoresvc",
+    "det-store"
+    )
+app.svcs += Svc(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/kernel/outstream:json_outstream_svc",
+    "outstreamsvc",
+    OutputLevel=Lvl.INFO
+    )
 
-app.algs += Alg("gaudi/tests/pkg2:alg_adder", "adder_1",
-                OutputLevel=Lvl.ERROR,
-                Val=0.)
-app.algs += Alg("gaudi/tests/pkg2:alg_dumper", "dumper-1",
-                ExpectedValue=1)
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg_adder",
+    "adder_1",
+    OutputLevel=Lvl.ERROR,
+    Val=0.
+    )
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg_dumper",
+    "dumper-1",
+    ExpectedValue=1
+    )
 
-app.algs += Alg("gaudi/tests/pkg2:alg_adder", "adder_2",
-                OutputLevel=Lvl.ERROR,
-                Val=3.)
-app.algs += Alg("gaudi/tests/pkg2:alg_dumper", "dumper-2",
-                ExpectedValue=2)
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg_adder",
+    "adder_2",
+    OutputLevel=Lvl.ERROR,
+    Val=3.
+    )
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg_dumper",
+    "dumper-2",
+    ExpectedValue=2
+    )
 
-app.algs += Alg("gaudi/tests/pkg2:alg_adder", "adder_3")
-app.algs += Alg("gaudi/tests/pkg2:alg_dumper", "dumper-3",
-                ExpectedValue=3)
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg_adder",
+    "adder_3"
+    )
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg_dumper",
+    "dumper-3",
+    ExpectedValue=3)
 
-app.algs += Alg("gaudi/tests/pkg2:alg_dumper", "dumper",
-                NbrJets = "njets",
-                ExpectedValue=3,
-                OutputLevel=Lvl.ERROR)
+app.algs += Alg(
+    "bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg2:alg_dumper",
+    "dumper",
+    NbrJets = "njets",
+    ExpectedValue=3,
+    OutputLevel=Lvl.ERROR)
 
-app.algs += Alg("gaudi/kernel/outstream:gob_outstream",
+app.algs += Alg("bitbucket.org/binet/ng-go-gaudi/gaudi/kernel/outstream:gob_outstream",
                 "gobwriter",
                 Items=["cnt",],
                 Output="/tmp/foo.gob")
 
-app.algs += Alg("gaudi/kernel/outstream:json_outstream",
+app.algs += Alg("bitbucket.org/binet/ng-go-gaudi/gaudi/kernel/outstream:json_outstream",
                 "json-writer-1",
                 Output="/tmp/foo.1.json")
 
-app.algs += Alg("gaudi/kernel/outstream:json_outstream",
+app.algs += Alg("bitbucket.org/binet/ng-go-gaudi/gaudi/kernel/outstream:json_outstream",
                 "json-writer-2",
                 Output="/tmp/foo.2.json")
 
-app.toolsvc += Tool("gaudi/tests/pkg1:tool1", name="tool1")
+app.toolsvc += Tool("bitbucket.org/binet/ng-go-gaudi/gaudi/tests/pkg1:tool1", name="tool1")
 
